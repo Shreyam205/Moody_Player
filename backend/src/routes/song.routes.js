08d5828cb52ce659fa899ea7ba204112a7f82f85
Shreyam Sharma <rajroy10008@gmail.com>
2025-08-29 14:27:02 +0530

    Integrated the cloud storage to upload files and can access them with the public links through our DB

--- backend/src/routes/song.routes.js
+++ backend/src/routes/song.routes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const multer = require('multer')
+const uploadFile = require('../service/storage.service')
 const router = express.Router()
+const songModel = require('../models/song.model')
 
 const upload = multer({storage:multer.memoryStorage()})
 
@@ -11,15 +13,34 @@ const upload = multer({storage:multer.memoryStorage()})
 // }
 
 
-router.post('/songs', upload.single("audio"),(req, res) => {
+router.post('/songs', upload.single("audio"), async(req, res) => {
     console.log(req.body);
     console.log(req.file);
+    const filedata = await uploadFile(req.file)
+    const song = await songModel.create({
+        title: req.body.title,
+        artist: req.body.artist,
+        audio: filedata.url,
+        mood: req.body.mood
+    })
     res.status(201).json({
         message: 'Song created successfully',
-        song: req.body
+        song: song
     })
 })
 
+router.get('/songs', async(req, res) => {
+  const {mood} = req.query
+
+  const songs = await songModel.find({
+    mood: mood
+  })
+
+  res.status(200).json({
+    message: "songs fetched successfully",
+    songs
+  })
+})
 
 
 
